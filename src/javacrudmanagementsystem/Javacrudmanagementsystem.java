58d82610726b2af230fde3763d9ed5e20ae6fd36
SajithKavinda99 <sajithbusiness094@gmail.com>
2024-05-04 16:35:47 +0530

    Initial commit

---
+++ src/javacrudmanagementsystem/Javacrudmanagementsystem.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javacrudmanagementsystem;
+
+/**
+ *
+ * @author dell
+ */
+public class Javacrudmanagementsystem {
+    public static void main(String[] args) {
+        
+      systemlogin start = new systemlogin();
+      start.setVisible(true);
+       
+        
+    }
+    
+}
